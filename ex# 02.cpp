c737a0f781cdd46666d6bf833da6d3e62f0e5606
TahaHassan9614 <tahasmarti9614@gmail.com>
2017-05-13 18:28:31 +0500

    Add files via upload

---
+++ ex# 02.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	
+	for(int i=0;i<=100;i++)
+	{
+		cout<<i<<endl;
+	
+	}
+	
+	for(int i=0;i<=100;i++)
+	{
+		cout<<"\n";
+	if(i%2==1)
+	{
+		cout<<i<<endl;
+	}
+	}
+	
+	cout<<"Enter number"<<endl;
+	cin>>a;
+	
+	
+	for(int i=a;i<=100;i++)
+	{
+		cout<<i<<endl;
+	}
+	
+
+}
---
+++ min max sum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,max,min;
+	
+	do
+	{
+	cout<<"Enter Max And Min number"<<endl;
+	cin>>max>>min;
+	
+	a=max+min;
+	cout<<a;
+	
+	}
+	
+	
+	while(max>min);
+	{
+		cout<<"Enter Again"<<endl;
+	}
+	
+		
+}
